cdc4aba388d0c2cd9c83b373d8d3bfc463793a98
uluagac <ersinuluagac@gmail.com>
2025-01-05 19:53:47 +0300

    apply page; controller, view and model

---
+++ ASP.NET Core MVC/Akademi/Controllers/CourseController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Akademi.Controllers
+{
+  public class CourseController : Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Apply()
+    {
+      return View();
+    }
+  }
+}
--- ASP.NET Core MVC/Basics/obj/Debug/net6.0/Basics.AssemblyInfo.cs
+++ ASP.NET Core MVC/Basics/obj/Debug/net6.0/Basics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Basics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1441b51acd89f0ef51489f4103569be2e7d7e8f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6da17f9e9212edfbd8a6813cc5cc847e3eb14de6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Basics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Basics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
