15191a3a9fd60a618a547588599d36c2283b417f
uluagac <ersinuluagac@gmail.com>
2025-02-04 21:48:42 +0300

    identity finished: cookies and taghelper

--- ASP.NET Core MVC/Store/StoreApp/Program.cs
+++ ASP.NET Core MVC/Store/StoreApp/Program.cs
@@ -9,6 +9,7 @@ builder.Services.ConfigureSession();
 builder.Services.ConfigureRepositoryRegistration();
 builder.Services.ConfigureServiceRegistration();
 builder.Services.ConfigureRouting();
+builder.Services.ConfigureApplicationCookie();
 builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
